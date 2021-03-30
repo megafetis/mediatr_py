4322726ce173f3d0827194ce5ba13495c5e6497c
Linus Nibell <linus.nibell@vinnter.se>
2021-03-30 12:48:11 +0200

    removed old test code

--- tests/test_class_handlers.py
+++ tests/test_class_handlers.py
@@ -5,7 +5,6 @@ from mediatr import Mediator
 from tests.example_handlers import common_log_behavior, get_array_query_behavior_3, get_array_query_behavior_6, \
     GetArrayQueryBehavior, \
     GetArrayQueryHandler
-from tests.example_handlers_annotations import GetArrayQueryHandlerWithAnnotations
 from tests.example_queries import GetArrayQuery1
 
 
@@ -39,25 +38,3 @@ class ClassHandlersTest(unittest.TestCase):
         self.assertEqual(query.items_count, 4)
         array_count = len(result)
         self.assertEqual(4, array_count)
-
-    # def test_3(self):
-    #     class Client:
-    #         def test_3(self):
-    #             return 'test_3'
-    #     get_array_query_handler_with_constructor = GetArrayQueryHandlerWithConstructor(Client)
-    #     Mediator.register_handler(get_array_query_handler_with_constructor)
-    #     query = GetArrayQuery1(5)
-    #     self.assertEqual(query.items_count, 5)
-    #     result = self.ioloop.run_until_complete(self.mediator.send_async(query))
-    #     self.assertEqual(query.items_count, 4)
-    #     array_count = len(result)
-    #     self.assertEqual(4, array_count)
-    
-    # def test_4(self):
-    #     Mediator.register_handler(GetArrayQueryHandlerWithAnnotations)
-    #     query = GetArrayQuery1(5)
-    #     self.assertEqual(query.items_count, 5)
-    #     result = self.ioloop.run_until_complete(self.mediator.send_async(query))
-    #     self.assertEqual(query.items_count, 4)
-    #     array_count = len(result)
-    #     self.assertEqual(4, array_count)
